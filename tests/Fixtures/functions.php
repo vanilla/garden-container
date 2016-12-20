b8d83a8db7735e7d9c0e39a63e3a8f483af9ed9e
Todd Burry <todd@vanillaforums.com>
2016-12-20 11:45:44 -0500

    Add the functions fixture

---
+++ tests/Fixtures/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2016 Vanilla Forums Inc.
+ * @license MIT
+ */
+
+namespace Garden\Container\Tests\Fixtures;
+
+/**
+ * Set the name on a db.
+ *
+ * @param Db $db The db to set.
+ * @param string $name The new name.
+ */
+function setDbName(Db $db, $name) {
+    $db->name = $name;
+}
